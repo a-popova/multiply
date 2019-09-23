b664c01f3ef69620f8d9e398551f31402b65773b
a-popova <papova.anastasiya.13@gmail.com>
2019-09-23 20:05:05 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  let firstArray = first.split('').reverse();
+  let secondArray = second.split('').reverse();
+  let result = [];
+  for (let i = 0; i < firstArray.length+secondArray.length; i++){
+    result.push(0);
+  }
+  for (let i = 0; i < firstArray.length; i++){
+    for (let j = 0; j < secondArray.length; j++){
+      result[i+j] += firstArray[i] * secondArray[j];
+    }
+  }
+  for (let i = 0; i < result.length - 1; i++){
+    result[i+1] += Math.floor(result[i] / 10);
+    result[i] = result[i] % 10;  
+  }
+  result = result.reverse();
+  if (result[0] === 0){
+    let removed = result.splice(0, 1);
+  } 
+  result = result.join("");
+  return result;
 }
